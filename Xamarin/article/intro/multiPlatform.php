476525a49298aca841f35a4f5739f2fca22c1c62
David Malý <malyce.dvd@gmail.com>
2016-09-26 11:27:41 +0200

    Multiplatformní vývoj, Xamarin
    Přidány nové články

--- Xamarin/article/intro/multiPlatform.php
+++ Xamarin/article/intro/multiPlatform.php
@@ -1,4 +1,34 @@
-<h2>Úvod do Xamarin</h2>
-<p>
-	Xamarin je .......
+<h2>Multiplatformní vývoj</h2>
+<p clas="introduction">
+	Mnoho aplikací v dnešní době je výsledkem jen vhodného spojení základních operací s daty (přidávání, editace a mazání), to vše v originálním grafickém designu. Z časového i finančního hlediska není efektiní tyto základní operace psát opětovně pro každou platformu. Psání jednoho kódu víckrát také zvyšuje jeho náročnost na údržbu.
 </p>
+<p>
+	Možností, jak napsat kód pro tu samou funkčnost pouze jednou, je hned několik.
+</p>
+<p>
+	Je možné je rozdělit do dvou základních kategorií, ty které:
+	<ol>
+		<li> využívají webové technologie </li>
+		<li> pracují na dané platformě nativně </li>
+	</ol>
+</p>
+<h3>Webové technologie</h3>
+<p>
+	Multiplatformní technologie kombinující HTML, CSS a Javascript tvořící vlastní framework. Jsou rychle rozvíjeny a staví na široké základně programátorů, kteří mají zkušenost s vývojem čistě webových aplikací. Výsledné aplikace jsou spouštěny ve WebView a neběží nativně.
+</p>
+<p> Zástupcem takových technologií jsou:</p>
+<ul>
+	<li><a href="https://cordova.apache.org/">Apache Cordova</a> </li>
+	<li><a href="http://www.phonegap.com">PhoneGap</a> </li>
+
+</ul>
+
+<h3>Multiplatformní - nativní technologie</h3>
+<p>
+	Technologie, které jsou kompilovány do nativních komponentů daných platforem a poskytují tak plnou integraci. Většinou se jedná o technologie, které jsou těžší na naučení, ale poskytují mnohem lepší nástroje pro čistě nativní vývoj. Výsledné aplikace běží naprosto nativně.
+</p>
+<p> Zástupci takových technologií jsou: </p>
+<ul>
+	<li><a href="https://facebook.github.io/react-native/">React Native</a> </li>
+	<li><a href="https://www.xamarin.com/">Xamarin</a> </li>
+</ul>
--- Xamarin/article/intro/xamarin.php
+++ Xamarin/article/intro/xamarin.php
@@ -1,4 +1,40 @@
-<h2>Úvod do Xamarin</h2>
-<p>
-	Xamarin je .......
+<h2>Xamarin</h2>
+<p class="introduction">
+	Na počátku byl Linux, na kterém neběžely stejné aplikace jako na Windows. Několik chytrých hlav se sešlo a přepsalo platformu .NET tak, aby běžela na Linuxu, vznikl projekt Mono.
 </p>
+
+<h3>Mono</h3>
+<p>
+	Mono je opensourcová implementace .NET frameworku sponzorovaná společností Microsoft. Mono je vyvíjeno pod ECMA standardy pro C# a CLR. Cílem tohoto projektu je vyvíjet multiplatformní aplikace, které běží na operačních systémech jako Linux, Unix, Mac OS X, Solaris a Windows atd.
+</p>
+
+<p>
+	Za vznikem projektu stojí Miguel de Icaza, který napsal vlastní kompilátor pro C#. Mono bylo oficiálně představeno na konferenci O'Reilly roku 2001. Protože je Mono open-source, tak kvůli vyvíjení vlastních produktů a licencování vznikla společnost Xamarin.
+</p>
+
+<p>
+	Po úspěšném zavedení Mona na osobní počítače se projekt vyvíjel dál směrem k Mono Touch a Mono for Android. Vývoj Mono převzala společnost Xamarin a přejmenovala dosavadní produty na Xamarin.Android, Xamarin.Mac atd. Společnost Xamarin licencovala své produkty (Mono produkty zůstaly pod open-source, Xamarin ne).
+</p>
+
+<h3>Xamarin - společnost</h3>
+<p>
+	V roce 2016 společnost Microsoft odkoupila společnost Xamarin a oznámalila, že Xamarin SDK budou open-source a budou dodávány zdarma všem (společnost Xamarin stále stojí za vývojem produkty Xamarin).
+</p>
+
+<h3>Xamarin - platforma</h3>
+<p>
+	Umožňuje psát aplikace v C# pro různé operační systémy, samotná implementace zdrojového kódu probíhá pomocí konkrétních Xamarin SDK. Pro vývoj na Android v C# pomocí Xamarin se využívá Xamarin.Android, pro iOS Xamarin.iOS atd.
+</p>
+<p> 
+	Kompilace probíhá stejně jako u .NET platformy,zdrojový kód se zkompiluje do přenositelného (CIL) a poté se na daném zařízení konkretizuje na nativní (dle CPU atd.).
+</p>
+<p>
+	Xamarin platforma se skládá z z několika částí:
+</p>
+<ul>
+	<li> C# - využívá velkou základnu programátorů, doplňků do VS, tutoriálů atd. </li>
+	<li> .NET - poskytuje obrovské množství knihoven, je to platforma stavěná na multiplatformní vývoj </li>
+	<li> Kompilátor - produkuje nativní kód založení na multiplatformním</li>
+</ul>
+
+<i> Pozn. <a href="https://developer.xamarin.com/guides/cross-platform/application_fundamentals/building_cross_platform_applications/part_1_-_understanding_the_xamarin_mobile_platform/"> Více o Xamarin </a></i>
